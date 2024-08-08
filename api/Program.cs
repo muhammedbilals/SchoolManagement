e2c1ebf93dfc1fcd313306a3221e95092c487c4a
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-08-08 22:00:14 +0530

    update on database connection

--- api/Program.cs
+++ api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>{
-    options.UseMySql(builder.Configuration.GetConnectionString(""),new MySqlServerVersion(new Version(8,0,25)));
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),new MySqlServerVersion(new Version(8,0,25)));
 
 });
 
