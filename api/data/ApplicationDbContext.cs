2ed51b79949841651ead68e9b7e74b260ce9da23
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-08-08 15:07:40 +0530

    database setup done

---
+++ api/data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
+
+        {
+            
+        }
+        public DbSet<User> Users {get;set;}
+        
+    }
+}
