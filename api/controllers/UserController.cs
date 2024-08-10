633fd78fea33f901c0aa7582d3b96864340e9b78
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-08-10 21:05:21 +0530

    created login methode

--- api/controllers/UserController.cs
+++ api/controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.data;
+using api.dtos.user;
 using api.mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,14 +20,27 @@ namespace api.controllers
        {
         _context =context;
        }
-       [HttpGet]
+       [HttpGet("getusers")]
         public IActionResult GetUsers(){
             var User = _context.Users.ToList().Select(s => s.ToUserDto());
+
             return Ok(User);
         }
-        // [HttpPost]
-        // public IActionResult Login([FromBody]  ){
-            
-        // }
+
+        
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginUserDto userDto ){
+            var userModel = userDto.ToLoginDto();
+            var user =_context.Users.FirstOrDefault(u => u.Email == userModel.Email);
+        if (user ==null){
+            return NotFound(new {message = "User not found"});
+        }
+
+        if (user.Password != userModel.Password){
+            return Unauthorized(new {message ="Wrong Password"});
+        }
+
+        return Ok(new {message ="Login Successfull", user = user.ToUserDto()});
+        }   
     }
 }
--- api/dtos/user/LoginUserDto.cs
+++ api/dtos/user/LoginUserDto.cs
@@ -7,10 +7,7 @@ namespace api.dtos.user
 {
     public class LoginUserDto
     {
-             public string Email { get; set; } =string.Empty;
-
-            public string Name { get; set; } =string.Empty;
-
+             public string Email { get; set; } =string.Empty;   
             public string Password { get; set; } =string.Empty;
     }
 
